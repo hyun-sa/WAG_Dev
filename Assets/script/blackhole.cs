5d50e33c6614882c0df17e41306383a86aa83a42
LEE026 <58213749+LEE026@users.noreply.github.com>
2020-08-11 12:26:13 +0900

    최초버전

---
+++ Assets/script/blackhole.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class blackhole : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("블랙홀에 닿아서 죽음");
+        }
+    }
+}
---
+++ Assets/script/blinkwall.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+
+public class blinkwall : MonoBehaviour
+{
+    [SerializeField]
+    private float BlinkTime=3;
+
+    private bool b = false;
+    private Vector3 pos;
+    private Vector2 size;
+    private GameObject player;
+    private void Awake()
+    {
+        player = GetComponent<setting>().Return_player();
+        size = gameObject.GetComponent<BoxCollider2D>().size/2;
+        check_time();
+    }
+
+
+    private void check_time()
+    {
+        b = !b;
+        gameObject.SetActive(b);
+        if (b)
+        {
+            pos = player.transform.position;
+            if (System.Math.Abs(transform.position.x-pos.x)<size.x&& System.Math.Abs(transform.position.y - pos.y) < size.y)
+            {
+                Debug.Log("끼어죽음");
+            }
+        }
+        Invoke("check_time", BlinkTime);
+    }
+}
